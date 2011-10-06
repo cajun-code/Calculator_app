8a7b5ed09d892a296cf60556f188e9735a9eef0c
Allan Davis <cajun.code@gmail.com>
2011-10-05 23:19:48 -0400

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Allan Davis on 10/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by Allan Davis on 10/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
